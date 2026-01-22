41d313b9c6d0280c14dff67eb9d59677861dc525
Saurav Kumar <sauravkr8293@gmail.com>
2026-01-22 22:02:02 +0530

    Refactor project structure: add day-01 folder, clean repo, add .gitignore and .env.example

--- day-01-basic-genai-app/app.py
+++ day-01-basic-genai-app/app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv(dotenv_path=".env")
+load_dotenv(dotenv_path="../.env")
 
 api_key = os.getenv("GOOGLE_API_KEY")
 print("API KEY =", api_key) 
@@ -11,12 +11,12 @@ client = genai.Client(api_key=api_key)
 
 response1 = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Explain Generative AI in simple words."
+    contents="Explain Generative AI in one sentence."
 )
 
 response2 = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Give me a simple example of Generative AI."
+    contents="Give me a simple one example of Generative AI."
 )
 
 print(response1.text)
